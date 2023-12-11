3ebc0ff4636dcf2131be87a627c2a077ad772a64
underscoren <marius.steffens@outlook.com>
2023-12-11 23:23:23 +0000

    fix: type

--- src/lib/server/mongo.ts
+++ src/lib/server/mongo.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const url = process.env.MONGO_URL ?? 'mongodb://126.0.0.1:27017';
+const url = process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
 
 export async function startMongoDB() {
